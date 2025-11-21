fd2ff2d834f0e514503835cb46aa69f5feb756d2
itsseanez <74938477+itsseanez@users.noreply.github.com>
2025-11-21 18:57:05 -0500

    Create math functions in script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let add = (x, y) => {
+    return Number(x) + Number(y);
+};
+
+let subtract = (x, y) => {
+    return Number(x) - Number(y);
+};
+
+let multiply = (x, y) => {
+    return Number(x) * Number(y);
+};
+
+let divide = (x, y) => {
+    return Number(x) / Number(y);
+};
+
+console.log(add(1,2))
